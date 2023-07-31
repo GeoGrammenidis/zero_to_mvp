20f015ba8f72463568125a8af71dc7344f897046
Grammenidis G <grammenidis@yahoo.gr>
2023-07-31 10:43:12 +0300

    2 small tests added

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -9,32 +9,28 @@ describe("End-to-End Tests", () => {
     page = await browser.newPage();
 
     // Navigate the page to a URL
-    await page.goto("http://127.0.0.1:8080/level_1.html");
+    await page.goto("http://127.0.0.1:8080/for_tests.html");
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
-
-    // // Type into search box
-    // await page.type(".search-box__input", "automate beyond recorder");
-
-    // // Wait and click on first result
-    // const searchResultSelector = ".search-box__link";
-    // await page.waitForSelector(searchResultSelector);
-    // await page.click(searchResultSelector);
-
-    // // Locate the full title with a unique string
-    // const textSelector = await page.waitForSelector(
-    //   "text/Customize and automate"
-    // );
-    // const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-    // console.log('The title of this blog post is "%s".', fullTitle);
   });
 
   afterAll(async () => {
     await browser.close();
   });
 
-  it("first test", async () => {
-    expect(1).toBe(1);
+  it("title", async () => {
+    expect.assertions(1);
+    const textSelector = await page.waitForSelector("title");
+    const title = await textSelector?.evaluate((el) => el.textContent);
+    expect(title).toBe("Page for tests!");
+  });
+
+  it("click", async () => {
+    expect.assertions(1);
+    await page.click("#first_button");
+    const appSelector = await page.waitForSelector("#app :first-child");
+    const app = await appSelector?.evaluate((el) => el.textContent);
+    expect(app).toBe("This is a new paragraph created with JavaScript!");
   });
 });
